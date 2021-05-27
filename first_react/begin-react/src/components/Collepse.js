750d925e31780cef0b2c8fefaf7cc69b57ea6377
sefammirza <81354116+sefammirza@users.noreply.github.com>
2021-05-27 22:07:04 +0300

    setState

--- first_react/begin-react/src/components/Collepse.js
+++ first_react/begin-react/src/components/Collepse.js
@@ -1,23 +1,61 @@
+
+// İlk Constructor ---> State initialization 
+//render meyodu ilk defa çalıştı
+//onClick iventi çalıştı
+//setState state nesnesini güncelledi --> triggered render
+//render metodu güncel JSX i Sayfada gösteriyor 
+
 import React from 'react';
 
+class Collepse extends React.Component {
 
+    constructor(){
+        super();
+        this.state = {
+            showContent:false
+        }
+     /*   this.showMore = () =>{
 
-const Collepse = (props) => {
-    return (
-        <div>
-                <a class="btn btn-primary w-100"  data-bs-toggle="collapse" href={"#".concat(props.href)} role="button" aria-expanded="false" aria-controls="collapseExample">
+        }*/
+        //this.showMore = this.showMore.bind(this)
+    }
+
+    /*showMore(){
+        this.setState({showContent: true})
+        //console.log(this)
+        
+    }*/
+
+    showMore = () => {
+    this.setState({showContent: !this.state.showContent}) 
+    //this.state = {
+    //showContent:true
+    //}
+    }
+
+    render() {
+        return (
+            <div>
+                <button className="btn btn-primary w-100" onClick={this.showMore }>
                     Link with href
-                </a>
-            <div class="collapse show" id={props.href}>
-                {props.children}
+                </button>
+                
+                {
+                    this.state.showContent ? (
+                        <div className="collapse show">
+                        {this.props.children}
+                    </div>
+                    ) :null
+                }
+
+
             </div>
-        </div>
-    );
+        );
+    }
+
 };
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
+
 
 
 export default Collepse;
