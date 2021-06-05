53730a526344407a01f8c3eac862b8c4f4dc1d8d
sefammirza <81354116+sefammirza@users.noreply.github.com>
2021-06-06 01:27:51 +0300

    our mmenu catagories

--- menu/src/App.js
+++ menu/src/App.js
@@ -3,10 +3,21 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories =['all',...new Set(items.map((item) => item.category))];
+
 function App() {
 
   const [menuItems, setMenuItems] = useState(items);
-  const [catagories, setCatagories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if(category === 'all'){
+      setMenuItems(items)
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return(
 
@@ -15,7 +26,7 @@ function App() {
       <div className="title">
         <h2>Our Menu</h2>
       </div>
-      <Categories/>
+      <Categories categories={categories} filterItems={filterItems} />
       <Menu items={menuItems}/>
     </section>
   </main>
