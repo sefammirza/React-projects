595640762036d1099e23c4d3e33bbb0717e19b97
sefammirza <81354116+sefammirza@users.noreply.github.com>
2021-06-07 23:28:21 +0300

    tags useEffect

--- tabs/src/App.js
+++ tabs/src/App.js
@@ -3,8 +3,33 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
+
+
+
 function App() {
-  return <h2>tabs project setup</h2>
+
+const [loading, setLoading] = useState(true);
+const [jops, setJops] = useState([]);
+const [value, setValue] = useState(0);
+
+const fetchJobs = async () => {
+const response = await fetch(url);
+const newJobs = await response.json();
+setJops(newJobs);
+setLoading(false);
+};
+
+useEffect(()=> {
+  fetchJobs();
+}, []);
+
+if (loading){
+  return (<section className='section loading'>
+    <h1>loading...</h1>
+  </section>);
+}
+
+  return <h2>jobs</h2>
 }
 
 export default App
