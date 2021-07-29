c15902b75a7450490cbb10637352a3905c2dcf0a
sefammirza <81354116+sefammirza@users.noreply.github.com>
2021-07-29 18:28:06 +0300

    footer react

---
+++ react-footer-yt-v1/src/components/footer/styles/footer.js
@@ -0,0 +1,19 @@
+ import styled from 'styled-components';
+
+ export const Container = styled.div`
+ 
+ padding:80px 60px;
+ background:radial-gradient(circle,rgba(92, 39, 251,1) 0%, rgba(112,71, 247,1) 100%)
+ 
+ `
+
+ export const Column
+  = styled.div``
+
+ export const Container = styled.div``
+
+ export const Container = styled.div``
+
+ export const Container = styled.div``
+
+ export const Container = styled.div``
