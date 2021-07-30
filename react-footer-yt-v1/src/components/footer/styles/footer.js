06109022edd249e53dcae36aded80e5fddf1480c
sefammirza <81354116+sefammirza@users.noreply.github.com>
2021-07-30 11:59:54 +0300

    react footer

--- react-footer-yt-v1/src/components/footer/styles/footer.js
+++ react-footer-yt-v1/src/components/footer/styles/footer.js
@@ -3,17 +3,51 @@
  export const Container = styled.div`
  
  padding:80px 60px;
- background:radial-gradient(circle,rgba(92, 39, 251,1) 0%, rgba(112,71, 247,1) 100%)
+ background:radial-gradient(circle,rgba(92, 39, 251,1) 0%, rgba(112,71, 247,1) 100%);
  
  `
 
- export const Column
-  = styled.div``
+ export const Wrapper = styled.div`
+ display:flex;
+ flex-direction:column;
+ justify-content:center;
+ max-width:1000px;
+ margin:0 auto;
+ `
 
- export const Container = styled.div``
+ export const Column = styled.div`
+ display:flex;
+ flex-direction:column;
+ text-align:left;
+ margin-left:60px;
+ `
 
- export const Container = styled.div``
+ export const Row = styled.div`
+ display:grid;
+ grid-template-columns:repeat(auto-fill, minmax(230px,1fr));
+ grid-gab:20px;
 
- export const Container = styled.div``
+ @media (max-width:1000px){
+     grid-template-columns::repeat(auto-fill, minmax(230px,1fr)); 
+    
+ }
+ `
 
- export const Container = styled.div``
+ export const Link = styled.div`
+ color:#fff;
+ margin-bottom:20px;
+ font-size:18px;
+ text-decoration:none;
+
+ &:hover{
+     color:#ff9c00;
+     transition:200ms ease-in;
+ }
+ `
+
+ export const Title = styled.div`
+ font-size:24px;
+ color:#fff;
+ margin-bottom:40px;
+ font-weight:bold;
+ `
--- react-footer-yt-v1/src/containers/footer.js
+++ react-footer-yt-v1/src/containers/footer.js
@@ -5,7 +5,36 @@ import Footer from '../components/footer'
 export function FooterContainer(){
     return(
         <Footer>
-            
+            <Footer.Wrapper>
+                <Footer.Row>
+                <Footer.Column>
+                    <Footer.Title>About Us</Footer.Title>
+                    <Footer.Link href="#">Story</Footer.Link>
+                    <Footer.Link href="#">Clients</Footer.Link>
+                    <Footer.Link href="#">Testimonials</Footer.Link>
+                </Footer.Column>
+                <Footer.Column>
+                    <Footer.Title>Services</Footer.Title>
+                    <Footer.Link href="#">Marketing</Footer.Link>
+                    <Footer.Link href="#">Consulting</Footer.Link>
+                    <Footer.Link href="#">Developments</Footer.Link>
+                    <Footer.Link href="#">Design</Footer.Link>
+                </Footer.Column>
+                <Footer.Column>
+                    <Footer.Title>Contact Us</Footer.Title>
+                    <Footer.Link href="#">Unatied States</Footer.Link>
+                    <Footer.Link href="#">Unatied Kingdom</Footer.Link>
+                    <Footer.Link href="#">Australia</Footer.Link>
+                    <Footer.Link href="#">Support</Footer.Link>
+                </Footer.Column>
+                <Footer.Column>
+                    <Footer.Title>Social</Footer.Title>
+                    <Footer.Link href="#">Facebook</Footer.Link>
+                    <Footer.Link href="#">Instragram</Footer.Link>
+                    <Footer.Link href="#">Twitter</Footer.Link>
+                </Footer.Column>
+                </Footer.Row>
+            </Footer.Wrapper>
         </Footer>
     )
 }
