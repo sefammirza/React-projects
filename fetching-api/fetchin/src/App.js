d9c2e1ee95435e3b01a705999c702079e08faebe
sefammirza <81354116+sefammirza@users.noreply.github.com>
2021-10-22 20:59:28 +0200

    fetching api

---
+++ fetching-api/fetchin/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DataFetching from './DataFetching';
+
+function App() {
+  return (
+    <div className="App">
+     <DataFetching/>
+    </div>
+  );
+}
+
+export default App;
