f0aa6efd63c1250df3cb23d604d6f3cd1fecd3c6
sefammirza <81354116+sefammirza@users.noreply.github.com>
2021-10-27 23:18:22 +0200

    square

--- tic-tac-toe/tic-tac-toe-tutorial/src/App.js
+++ tic-tac-toe/tic-tac-toe-tutorial/src/App.js
@@ -6,6 +6,20 @@ function App() {
   const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""])
   const [player, setPlayer] = useState("X")
   const chooseSquare = (square) => {
+    setBoard(board.map((val, idx) => {
+      if (idx == square && val == "") {
+        return player
+      }
+      return val
+    }))
+
+    if (player == "X"){
+      setPlayer("O")
+    }
+    else {
+      setPlayer ("X") 
+    }
+
 
   }
   return (
@@ -26,8 +40,34 @@ function App() {
             chooseSquare={() => {chooseSquare(2)}}
           />
         </div>
-        <div className="row"></div>
-        <div className="row"></div>
+        <div className="row">
+        <Square 
+          val={board[3]} 
+          chooseSquare={() => {chooseSquare(3)}}
+            />
+          <Square
+            val={board[4]} 
+            chooseSquare={() => {chooseSquare(4)}}
+          />
+          <Square
+            val={board[5]} 
+            chooseSquare={() => {chooseSquare(5)}}
+          />
+        </div>
+        <div className="row">
+        <Square 
+          val={board[6]} 
+          chooseSquare={() => {chooseSquare(6)}}
+            />
+          <Square
+            val={board[7]} 
+            chooseSquare={() => {chooseSquare(7)}}
+          />
+          <Square
+            val={board[8]} 
+            chooseSquare={() => {chooseSquare(8)}}
+          />
+        </div>
       </div>
     </div>
   );
