5caaab2a9f45605f4f1e7c9e6282ea20d87d9ff2
sefammirza <81354116+sefammirza@users.noreply.github.com>
2022-01-12 22:45:08 +0100

    firebase-react-auth

---
+++ firebase-react-auth/src/components/Signup.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Form, Button, Card } from 'react-bootstrap'
+
+export default function Signup() {
+    return (
+        <>
+            <Card></Card>
+        </>
+    )
+}
