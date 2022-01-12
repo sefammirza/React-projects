4473980cd17500930cc0b65da16300ef37f17e1d
sefammirza <81354116+sefammirza@users.noreply.github.com>
2022-01-13 00:45:25 +0100

    signup

--- firebase-react-auth/src/components/App.js
+++ firebase-react-auth/src/components/App.js
@@ -1,7 +1,14 @@
+import Signup from "./Signup";
+import React from "react";
+import { Container } from "react-bootstrap";
 
 
 function App() {
-  return "Hello"
+  return (
+    <Container className="d-flex align-items-center justify-content-center">
+      <Signup/>
+    </Container>
+  )
 }
 
 export default App;
