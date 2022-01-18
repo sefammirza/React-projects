2159531da924b45eb9750d96dd953e8dd112b2d5
sefammirza <81354116+sefammirza@users.noreply.github.com>
2022-01-18 23:46:41 +0300

    container

--- firebase-react-auth/src/components/App.js
+++ firebase-react-auth/src/components/App.js
@@ -5,8 +5,10 @@ import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <Container className="d-flex align-items-center justify-content-center">
-      <Signup/>
+    <Container className="d-flex align-items-center justify-content-center" style={{minHeight:"100vh"}}>
+      <div className="w-100" style={{maxWidth:"400px"}}>
+        <Signup/>
+      </div>
     </Container>
   )
 }
