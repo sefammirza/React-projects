df9ec6f13ec19961702f158a97b32580d4195507
sefammirza <81354116+sefammirza@users.noreply.github.com>
2022-01-21 22:29:28 +0300

    handlesubmit

--- firebase-react-auth/src/components/App.js
+++ firebase-react-auth/src/components/App.js
@@ -1,15 +1,19 @@
 import Signup from "./Signup";
 import React from "react";
 import { Container } from "react-bootstrap";
+import {AuthProvider} from '../context/AuthContext'
 
 
 function App() {
   return (
-    <Container className="d-flex align-items-center justify-content-center" style={{minHeight:"100vh"}}>
+    <AuthProvider>
+         <Container className="d-flex align-items-center justify-content-center" style={{minHeight:"100vh"}}>
       <div className="w-100" style={{maxWidth:"400px"}}>
         <Signup/>
       </div>
-    </Container>
+    </Container> 
+    </AuthProvider>
+
   )
 }
 
--- firebase-react-auth/src/context/AuthContext.js
+++ firebase-react-auth/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import React , {useContext, useState} from 'react'
+import React , {useContext, useState, useEffect} from 'react'
 import {auth} from '../firebase'
 
 const AuthContext = React.createContext()
@@ -14,8 +14,24 @@ export function AuthProvider({children}) {
 
     const [currentUser, setCurrentUser] = useState()
 
+    function signup(email, password) {
+        return auth.createUserWithEmailAndPassword(email, password)
+      }
+
+    useEffect(() => {
+     const unsubscribe = auth.onAuthStateChanged(user => {
+        setCurrentUser(user)
+    })   
+
+    return unsubscribe 
+
+    }, [])
+
+    
+
     const value = {
-        currentUser
+        currentUser,
+        signup
     }
 
 
