5d8d487fea92cd018d1c84caef8f33f9652e25c5
sefammirza <81354116+sefammirza@users.noreply.github.com>
2022-03-02 23:15:17 +0100

    getPlacesData

--- travel_advisor/src/App.js
+++ travel_advisor/src/App.js
@@ -5,13 +5,12 @@ import {getPlacesData} from './api'
 import Header from './components/Header/Header';
 import List from './components/List/List'
 import Map from './components/Map/Map'
-import PlaceDetails from './components/PlaceDetails/PlaceDetails'
+
 
 
 const App = () => {
 
     const [places, setPlaces] = useState([])
-
     const [coordinates, setCoordinates] = useState({});
     const [bounds, setBounds] = useState(null);
 
@@ -23,9 +22,8 @@ const App = () => {
 
     useEffect(() => {
 
-  /*       getPlacesData(bounds.sw, bounds.ne)
+        /* getPlacesData(bounds.sw, bounds.ne)
         .then((data) => {
-            console.log(data)
             setPlaces(data)
         }) */
     }, [coordinates, bounds]);
--- travel_advisor/src/components/List/List.jsx
+++ travel_advisor/src/components/List/List.jsx
@@ -1,27 +1,15 @@
 import React, {useState} from "react";
 import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from "@material-ui/core";
-
 import PlaceDetails from '../PlaceDetails/PlaceDetails'
-
 import useStyles from './styles'
 
-const List = () => {
+const List = ({places}) => {
 
     const classes = useStyles();
     const [type, setType] = useState('restaurants');
     const [rating, setRating] = useState('');
 
-    const places = [
-        {name: 'Cool Place'},
-        {name: 'Best Beer'},
-        {name: 'Best Steak'},
-        {name: 'Cool Place'},
-        {name: 'Best Beer'},
-        {name: 'Best Steak'},
-        {name: 'Cool Place'},
-        {name: 'Best Beer'},
-        {name: 'Best Steak'},
-    ]
+ 
 
     return (
         <div className={classes.container}>
