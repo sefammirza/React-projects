16d44db1ef6051bb59cfb956e000ad5fdc4fad2f
sefammirza <81354116+sefammirza@users.noreply.github.com>
2022-03-04 23:45:11 +0100

    Typography

--- travel_advisor/src/components/PlaceDetails/PlaceDetails.jsx
+++ travel_advisor/src/components/PlaceDetails/PlaceDetails.jsx
@@ -13,8 +13,16 @@ const PlaceDetails = ({place}) => {
             style={{height:350}}
             image={place.photo ? place.photo.images.large.url : 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.imgacademy.com%2Fabout-img-academy&psig=AOvVaw07nAcOtR644JHRDxVEOdS9&ust=1646432962301000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKD3nPH-qvYCFQAAAAAdAAAAABAD'}
             title={place.name}
-            
             />
+            <CardContent>
+                <Typography gutterBottom variant="h5">
+                    {place.name}
+                </Typography>
+                <Box display='flex' justifyContent="space-between">
+                    <Typography variant="subtitle1">Price</Typography>
+                    <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
+                </Box>
+            </CardContent>
         </Card>
     );
 }
