4d48020afd492c2618c64405c5430805de2515df
sefammirza <81354116+sefammirza@users.noreply.github.com>
2022-03-06 23:23:16 +0100

    latitude

--- travel_advisor/src/App.js
+++ travel_advisor/src/App.js
@@ -21,7 +21,7 @@ const App = () => {
     }, [])
 
     useEffect(() => {
-        getPlacesData(bounds.sw, bounds.ne)
+        getPlacesData()
         .then((data) => {
             setPlaces(data)
         })
--- travel_advisor/src/api/index.js
+++ travel_advisor/src/api/index.js
@@ -8,7 +8,7 @@ export const getPlacesData = async (sw, ne) => {
     try {
         const {data: {data}} = await axios.get(URL, {
           params: {
-            bl_latitude:sw.lat ,
+            bl_latitude: sw.lat,
             tr_latitude: ne.lat,
             bl_longitude: sw.lng,
             tr_longitude: ne.lng,
