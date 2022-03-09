10106035358438f009c4731fc8889da687e2ca07
sefammirza <81354116+sefammirza@users.noreply.github.com>
2022-03-09 23:29:43 +0100

    getplacesdata

--- travel_advisor/src/api/index.js
+++ travel_advisor/src/api/index.js
@@ -6,7 +6,7 @@ const URL ='https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
 export const getPlacesData = async (sw, ne) => {
     try {
-        const {data: {data}} = await axios.get(URL, {
+/*         const {data: {data}} = await axios.get(URL, {
           params: {
             bl_latitude: sw.lat,
             tr_latitude: ne.lat,
@@ -16,7 +16,7 @@ export const getPlacesData = async (sw, ne) => {
           headers: {
             'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
             'x-rapidapi-key': '64d47e031fmshb3b4605d2ca27e7p1093acjsn4c01ad106b3d'
-          }
+          } */
         });
 
         return data;
